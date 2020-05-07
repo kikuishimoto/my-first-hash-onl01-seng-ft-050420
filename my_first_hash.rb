1f165ba35052c99d990a58648cccfc3de8e7d06c
kiku ishimoto <kikuish@gmail.com>
2020-05-07 00:38:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,9 +23,9 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
-end
 shipping_manifest['oil paintings']
+end
+
 
 def adding
   shipping_manifest = {
